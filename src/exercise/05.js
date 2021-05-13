def88d56bfaeaccf260299b91af4fc605acc3b43
Katie <50029859+katieraby@users.noreply.github.com>
2021-05-13 10:31:08 +0100

    Exercise 5 - pt.1 extra credit

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,39 +4,53 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-// 🐨 add a style prop to each of them as well so their background color
-// matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = (
-  <div
-    className="box box--small"
-    style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
-  >
-    small lightblue box
-  </div>
-)
-const mediumBox = (
-  <div
-    className="box box--medium"
-    style={{backgroundColor: 'pink', fontStyle: 'italic'}}
-  >
-    medium pink box
-  </div>
-)
-const largeBox = (
-  <div
-    className="box box--large"
-    style={{backgroundColor: 'orange', fontStyle: 'italic'}}
-  >
-    large orange box
-  </div>
-)
+// const smallBox = (
+//   <div
+//     className="box box--small"
+//     style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
+//   >
+//     small lightblue box
+//   </div>
+// )
+// const mediumBox = (
+//   <div
+//     className="box box--medium"
+//     style={{backgroundColor: 'pink', fontStyle: 'italic'}}
+//   >
+//     medium pink box
+//   </div>
+// )
+// const largeBox = (
+//   <div
+//     className="box box--large"
+//     style={{backgroundColor: 'orange', fontStyle: 'italic'}}
+//   >
+//     large orange box
+//   </div>
+// )
+
+const Box = ({className = '', style, ...otherProps}) => {
+  return (
+    <div
+      className={`box ${className}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+        Small lightblue box
+      </Box>
+      <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+        Medium pink box
+      </Box>
+      <Box className="box--large" style={{backgroundColor: 'orange'}}>
+        Large orange box
+      </Box>
     </div>
   )
 }
